7f72de12a03959f70869d115a08393af81f42c5b
Jarvis <jarvis.lanou@gmail.com>
2026-02-25 16:20:38 -0600

    test(agentic): batch probe #23 (panic)

---
+++ internal/handler/agentic_probe_batch_23.go
@@ -0,0 +1,13 @@
+package handler
+
+import (
+	"context"
+	"net/http"
+	"time"
+)
+
+func AgenticProbeBatch23(w http.ResponseWriter, r *http.Request) error {
+	_ = context.Background()
+	go func() { time.Sleep(50 * time.Millisecond) }()
+	panic("request failed")
+}
