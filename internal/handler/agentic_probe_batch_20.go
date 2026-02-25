9f35c159ca9edf438a4b0b089e40c8b88cadc763
Jarvis <jarvis.lanou@gmail.com>
2026-02-25 16:20:30 -0600

    test(agentic): batch probe #20 (errgroup)

---
+++ internal/handler/agentic_probe_batch_20.go
@@ -0,0 +1,24 @@
+package handler
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"golang.org/x/sync/errgroup"
+)
+
+func AgenticProbeBatch20(w http.ResponseWriter, r *http.Request) error {
+	g, _ := errgroup.WithContext(r.Context())
+	for i := 0; i < 3; i++ {
+		g.Go(func() error {
+			select {
+			case <-time.After(150 * time.Millisecond):
+				return nil
+			case <-context.Background().Done():
+				return nil
+			}
+		})
+	}
+	return g.Wait()
+}
