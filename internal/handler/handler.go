19e2bdac476aa8cc82dd8594f53a45af898d2b15
Jarvis <jarvis.lanou@gmail.com>
2026-02-24 20:30:26 -0600

    Add task duplication endpoint and UI action with tests

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -40,6 +40,7 @@ func (h *Handler) Register(e *echo.Echo) {
 	protected.POST("/tasks", h.CreateTask)
 	protected.GET("/tasks/:id/edit", h.EditTask)
 	protected.PUT("/tasks/:id", h.UpdateTask)
+	protected.POST("/tasks/:id/duplicate", h.DuplicateTask)
 	protected.DELETE("/tasks/:id", h.DeleteTask)
 	protected.PUT("/tasks/:id/move", h.MoveTask)
 }
